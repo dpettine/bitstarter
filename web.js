a65e5abdc2ce80d4fd6aaa808cbaf7f64e823994
Daniele Pettine <dpettine@gmail.com>
2013-07-08 22:34:14 +0000

    fixed fs

--- web.js
+++ web.js
@@ -1,11 +1,17 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var infile = "index.html";
+
+var enc = "utf-8";
+
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('index.html'));
+var buffer = new Buffer(fs.readFileSync(infile,enc),enc);
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(buffer.toString(enc));
 });
 
 var port = process.env.PORT || 5000;
